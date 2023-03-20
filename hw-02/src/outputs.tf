2aae274904ae09c52cfdee8c52ea710abee2c90b
vladislav.brattsev <Vlad_brat@mail.ru>
2023-03-20 15:11:57 +0000

    9

--- hw-02/src/outputs.tf
+++ hw-02/src/outputs.tf
@@ -1,11 +1,7 @@
-#output "develop" {
-#  value = yandex_vpc_network.develop.id.network_interface.0.nat_ip_address
-#}
+output "yandex_compute_instance" {
+    value = yandex_compute_instance.platform.network_interface[0].nat_ip_address
+}
 
-#output "external_ip" {
-#  value = network_interface_0.nat_ip_address
-#}
-
-output "default_instance_public_ip" {
-    value = yandex_compute_instance.default.network_interface[0].nat_ip_address
+output "yandex_compute_instance2" {
+    value = yandex_compute_instance.platform2.network_interface[0].nat_ip_address
 }
