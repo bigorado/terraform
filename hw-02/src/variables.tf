9d613383712d1c2b3bef08a77b11bbe19df99367
vladislav.brattsev <Vlad_brat@mail.ru>
2023-03-20 06:30:04 +0000

    2

--- hw-02/src/variables.tf
+++ hw-02/src/variables.tf
@@ -36,6 +36,7 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
-  description = "ssh-keygen -t ed25519"
+  default     = "/root/.ssh/id_rsa"
+#  default     = "<your_ssh_ed25519_key>"
+#  description = "ssh-keygen -t ed25519"
 }
